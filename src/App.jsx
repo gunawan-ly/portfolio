842b84216a2b3870f676dced36b3dced8b114ed8
adektersayang88 <adektersayang88@gmail.com>
2024-06-05 11:14:42 +0000

    v1.2

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Deploy by <b>Awan</b>
         </p>
       </div>
       <p className="read-the-docs">
