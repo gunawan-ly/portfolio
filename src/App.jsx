31a269ca2b97e213a3de1621019212b027f2eb0c
adektersayang88 <adektersayang88@gmail.com>
2024-06-05 15:26:08 +0000

    v1.6 Menambahkan component Jumbotroon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // import { useState } from "react";
 import Navbar from "./components/Navbar";
-// import "./App.css";
+import Jumbotron from "./components/Jumbotron";
+
 
 function App() {
   return (
     <>
     <Navbar/>
+    <Jumbotron/>
     </>
   )
 }
---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,23 @@
+export default function Jumbotron() {
+  return (
+    <>
+      <section>
+        <div class="jumbotron jumbotron-fluid">
+          <div class="container text-center">
+            <img
+              src="https://source.unsplash.com/random/100x100"
+              alt="No Image Link"
+              className="img-thumbnail rounded-circle mx-auto d-block m-2"
+            />
+
+            <h1 class="display-4">Awanophile</h1>
+            <p class="lead">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores,
+              dolores?
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
